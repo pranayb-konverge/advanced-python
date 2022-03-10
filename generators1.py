c3f483d0a8f97e25a2ea4d3a6be7f415f7110c83
Pranay Bankar <pranay.bankar@konverge.ai>
2022-03-10 22:25:06 +0530

    add generators, threading and variable files

---
+++ generators1.py
@@ -0,0 +1,14 @@
+print('\n----------------------generator function-----------------------------\n')
+# generator function
+
+import sys
+
+list_even_num = [x for x in range(1000000) if x%2==0]
+gen_even_num = (x for x in range(1000000) if x%2==0)
+
+print(sum(list_even_num), "Sum")
+print(sys.getsizeof(list_even_num), "bytes")
+print()
+print(sum(gen_even_num), "Sum")
+print(sys.getsizeof(gen_even_num), "bytes") # less memory space
+
